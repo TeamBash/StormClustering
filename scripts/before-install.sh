2f3a5a107a8068521196aebc165f02001cfeda4f
Manikandan Murugesan <manikandan.m@live.co.uk>
2016-11-15 20:34:02 -0500

    Fixing Travis for new setup

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -1,13 +1,3 @@
-echo 'check if maven is installed'
-export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk
-mvn --version
-if [ "$?" -ne 0 ]; then
-    echo 'Installing Maven...'
-	sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
-	sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-	sudo yum install -y apache-maven
-	mvn --version
-fi
 echo 'Checking if Docker is installed'
 docker --version
 if [ "$?" -ne 0 ]; then
@@ -23,4 +13,5 @@ if [ "$?" -ne 0 ]; then
 	chmod +x /usr/local/bin/docker-compose
 fi
 echo 'Removing existing docker instances' >> /var/log/sga-docker.log 2>&1
-docker ps -a | grep 'storm-clustering-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm -f storm-clustering-service
+docker ps -a | grep 'storm-clustering-service' | awk '{print $1}' | xargs --no-run-if-empty docker stop
+docker ps -a | grep 'storm-clustering-service' | awk '{print $1}' | xargs --no-run-if-empty docker rm
